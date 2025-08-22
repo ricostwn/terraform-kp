1911e8cc5a60f6216cba6383bdec8f9a3004b6e0
Rafi Nur Ardiansyah <rafimaster9@gmail.com>
2025-08-22 15:05:36 +0700

    refactor(firewall): reorganize firewall rules for ICMP and SSH, update port configurations

--- firewall.tf
+++ firewall.tf
@@ -1,16 +1,3 @@
-resource "google_compute_firewall" "allow_icmp" {
-  name    = "terraform-allow-icmp"
-  network = google_compute_network.vpc_network.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
-}
-
 resource "google_compute_firewall" "allow_internal" {
   name    = "terraform-allow-internal"
   network = google_compute_network.vpc_network.name
@@ -34,6 +21,19 @@ resource "google_compute_firewall" "allow_internal" {
   priority      = 65534
 }
 
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "terraform-allow-icmp"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  priority      = 65534
+}
+
 resource "google_compute_firewall" "allow_ssh" {
   name    = "terraform-allow-ssh"
   network = google_compute_network.vpc_network.name
@@ -46,22 +46,6 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = ["0.0.0.0/0"]
   direction     = "INGRESS"
   priority      = 65534
-}
-
-resource "google_compute_firewall" "allow_grafana" {
-  name    = "terraform-allow-grafana"
-  network = google_compute_network.vpc_network.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["3000"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
-
-  target_tags = ["monitoring"]
 }
 
 # Firewall rule for web traffic (HTTP/HTTPS)
@@ -81,37 +65,52 @@ resource "google_compute_firewall" "allow_web" {
   target_tags = ["monitoring"]
 }
 
+# resource "google_compute_firewall" "allow_grafana" {
+#   name    = "terraform-allow-grafana"
+#   network = google_compute_network.vpc_network.name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["3000"]
+#   }
+
+#   source_ranges = ["0.0.0.0/0"]
+#   direction     = "INGRESS"
+#   priority      = 65534
+
+#   target_tags = ["monitoring"]
+# }
 
 # Firewall rule for Prometheus
-resource "google_compute_firewall" "allow_prometheus" {
-  name    = "terraform-allow-prometheus"
-  network = google_compute_network.vpc_network.name
+# resource "google_compute_firewall" "allow_prometheus" {
+#   name    = "terraform-allow-prometheus"
+#   network = google_compute_network.vpc_network.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["9090"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["9090"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
+#   source_ranges = ["0.0.0.0/0"]
+#   direction     = "INGRESS"
+#   priority      = 65534
 
-  target_tags = ["monitoring"]
-}
+#   target_tags = ["monitoring"]
+# }
 
 # Firewall rule for Alertmanager
-resource "google_compute_firewall" "allow_alertmanager" {
-  name    = "terraform-allow-alertmanager"
-  network = google_compute_network.vpc_network.name
+# resource "google_compute_firewall" "allow_alertmanager" {
+#   name    = "terraform-allow-alertmanager"
+#   network = google_compute_network.vpc_network.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["9093"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["9093"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-  priority      = 65534
+#   source_ranges = ["0.0.0.0/0"]
+#   direction     = "INGRESS"
+#   priority      = 65534
 
-  target_tags = ["monitoring"]
-}
+#   target_tags = ["monitoring"]
+# }
